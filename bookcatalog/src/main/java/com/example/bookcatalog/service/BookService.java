06ea42fb355fab358d58b84e6a25a699658c977f
salahoula <salahoula35@gmail.com>
2025-05-03 14:41:38 +0100

    first commit

--- bookcatalog/src/main/java/com/example/bookcatalog/service/BookService.java
+++ bookcatalog/src/main/java/com/example/bookcatalog/service/BookService.java
@@ -7,12 +7,12 @@ import org.springframework.data.domain.Pageable;
 import java.util.List;
 
 public interface BookService {
-    List<Book> findAllBooks();
-    Book findBookById(Long id);
-    List<Book> searchBooks(String keyword);
-    Page<Book> findBooksPaginated(int page, int size, String sortBy, String direction);
-    List<Book> findBooksByYear(int year);
-    Book createBook(Book book);
-    Book updateBook(Long id, Book bookDetails);
-    void deleteBook(Long id);
+   // List<Book> findAllBooks();
+    // Book findBookById(Long id);
+   //  List<Book> searchBooks(String keyword);
+   //  Page<Book> findBooksPaginated(int page, int size, String sortBy, String direction);
+   //  List<Book> findBooksByYear(int year);
+   //  Book createBook(Book book);
+    // Book updateBook(Long id, Book bookDetails);
+     // void deleteBook(Long id);
 }
